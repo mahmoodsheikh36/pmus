5a18dabf11ab1b7effa32e88f8b1877eb502d0e6
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-21 02:42:57 +0200

    add "add" action to add to queue

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 music_object_type="${1:-song}"
+action="${2:-play}" # available options: add, play
 
 dmenu_out="$(music_daemon_cmd.sh list $music_object_type\
     | sort -n | rofi -dmenu -i -p ${music_object_type}s -multi-select)"
@@ -9,4 +10,4 @@ ids=""
 for id in "$(echo "$dmenu_out" | cut -d ' ' -f1)"; do
     ids="$ids $id"
 done
-music_daemon_cmd.sh play $music_object_type $ids
+music_daemon_cmd.sh $action $music_object_type $ids
