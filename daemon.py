017b21fa4c278f58289b3491197072d946062f82
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-21 02:42:35 +0200

    make daemon terminate properly

--- daemon.py
+++ daemon.py
@@ -2,6 +2,8 @@
 import sys
 import time
 import traceback
+import signal
+import os
 
 from music_daemon.player import MusicPlayer
 from music_daemon.db import MusicProvider
@@ -12,14 +14,18 @@ if __name__ == '__main__':
     library = provider.music()
     player = MusicPlayer()
     server = Server(player, library)
+
+    def on_exit(signum=None, frame=None):
+        server.terminate()
+        player.terminate()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, on_exit)
+    signal.signal(signal.SIGTERM, on_exit)
     
     try:
         server.start()
     except Exception as err:
         traceback.print_tb(err.__traceback__)
         print(err)
-    except KeyboardInterrupt:
-        print('interrupted, terminating..')
-
-    server.terminate()
-    player.terminate()
+        on_exit()
