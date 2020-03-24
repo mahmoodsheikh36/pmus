747b8dd04fa0d76c7d260b12aebb13cd44e5980b
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-24 19:45:54 +0200

    change interpreter to /usr/bin/python3

--- tests/secs_listened_to_song.py
+++ tests/secs_listened_to_song.py
@@ -1,10 +1,10 @@
-#!/bin/python
+#!/usr/bin/python3
 import sys
 sys.path.insert(1, '../music_daemon/')
 
 from music_daemon.db import MusicProvider
 
-SONG_ID = 1032
+SONG_ID = 61
 
 if __name__ == '__main__':
     provider = MusicProvider()
