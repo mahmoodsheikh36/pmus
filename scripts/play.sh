f66984422563a5e1ba95f105edb0b605dd552cfa
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-24 02:35:46 +0200

    handle more exit codes from rofi for additional actions

--- scripts/play.sh
+++ scripts/play.sh
@@ -4,16 +4,20 @@ music_object_type="${1:-song}"
 action="${2:-play}" # available options: add, play
 
 rofi_out="$(music_daemon_cmd.sh list $music_object_type\
-    | sort -n | rofi -dmenu -i -p ${music_object_type}s -multi-select)"
+    | sort -n | rofi -dmenu -i -p "${music_object_type}s" -multi-select)"
 
-# for now expand only works with albums, will be compatible with other
-# song lists in the future
-if [ $? -eq 10 ] && [ "$music_object_type" = "album" ]; then
+# for now expand only works with albums, will be made compatible with other
+# song lists like playlists in the future
+rofi_exit_code=$?
+if [ $rofi_exit_code -eq 10 ] && [ "$music_object_type" = "album" ]; then
     id=$(echo "$rofi_out" | cut -d ' ' -f1 | head -1)
     rofi_out="$(music_daemon_cmd.sh list ${music_object_type} $id\
         | sort -n | rofi -dmenu -i -p songs -multi-select)"
     music_object_type="song"
 fi
+
+[ $rofi_exit_code -eq 11 ] && action=play
+[ $rofi_exit_code -eq 12 ] && action=add
 
 ids=""
 for id in "$(echo "$rofi_out" | cut -d ' ' -f1)"; do
