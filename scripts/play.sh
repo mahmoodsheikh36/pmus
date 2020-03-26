8579feebef91779a041f0adbc4cc4a474276923a
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-26 22:44:58 +0200

    removed usage of sort before displaying response

--- scripts/play.sh
+++ scripts/play.sh
@@ -3,7 +3,7 @@
 music_object_type="${1:-song}"
 action="${2:-play}" # available options: add, play
 
-rofi_out="$(music_daemon_cmd.sh list $music_object_type | sort -n |\
+rofi_out="$(music_daemon_cmd.sh list $music_object_type |\
     rofi -async-pre-read 1 -dmenu -i -p "${music_object_type}s" -multi-select)"
 
 # for now expand only works with albums, will be made compatible with other
@@ -12,7 +12,7 @@ rofi_exit_code=$?
 if [ $rofi_exit_code -eq 10 ] && [ "$music_object_type" = "album" ]; then
     id=$(echo "$rofi_out" | cut -d ' ' -f1 | head -1)
     rofi_out="$(music_daemon_cmd.sh list ${music_object_type} $id\
-        | sort -n | rofi -async-pre-read 1 -dmenu -i -p songs -multi-select)"
+        | rofi -async-pre-read 1 -dmenu -i -p songs -multi-select)"
     rofi_exit_code=$?
     music_object_type="song"
 fi
