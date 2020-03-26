45e5b729940c4af45dc82a2a62684f64b96f1e48
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-26 22:45:26 +0200

    change mdc to music_daemon_cmd.sh, aliases cant and shouldnt be used in scripts

--- scripts/like_current_song.sh
+++ scripts/like_current_song.sh
@@ -3,5 +3,5 @@
 # the liked songs list in the remote database
 
 current_song_id="$(music_daemon_cmd.sh current | cut -d ' ' -f1)"
-mdc like $current_song_id
+music_daemon_cmd.sh like $current_song_id
 notify-send "liked song $current_song_id"
