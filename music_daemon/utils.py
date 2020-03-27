1ea8817fad82fdc1eae9fef079bfee2e6bc685ba
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-27 21:41:52 +0300

    add function to check if file exists

--- music_daemon/utils.py
+++ music_daemon/utils.py
@@ -1,5 +1,9 @@
 import time
 import subprocess
+import os.path
 
 # time since the unix epoch in milliseconds
 current_time = lambda: int(round(time.time() * 1000))
+
+def file_exists(filepath):
+    return os.path.isfile(filepath)
