1d6b1a88876afa9019c383434b3a7197baffd0df
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-02 13:49:01 +0300

    correct description

--- scripts/like_current_song.sh
+++ scripts/like_current_song.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# a script to like the current song that i am listening to and add it to
-# the liked songs list in the remote database
+# a script to like the current song that i am listening to
 
 current_song_id="$(music_daemon_cmd.sh current | cut -d ' ' -f1)"
 music_daemon_cmd.sh like $current_song_id
