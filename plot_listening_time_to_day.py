be88855f1bcade55520e0c301d2a877e4fbaf8b9
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-02 13:32:18 +0300

    change title, remove unnecessary line

--- plot_listening_time_to_day.py
+++ plot_listening_time_to_day.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     provider.load_music()
     first_time_listening = provider.playbacks[1].time_started
 
-    hours_cnt = 24 * 6
+    hours_cnt = 24 * 5
     times_listened = []
 
     from_time = first_time_listening
@@ -22,7 +22,6 @@ if __name__ == '__main__':
         total_time_listened = 0
         for song in provider.get_songs_list():
             total_time_listened += song.time_listened(from_time, to_time)
-        total_time_listened / 1000
         times_listened.append(total_time_listened)
         from_time = to_time
         to_time = next_hour(from_time)
@@ -32,5 +31,5 @@ if __name__ == '__main__':
             [time_listened / 1000 for time_listened in times_listened])
     ax.set_xlabel('hour', fontsize=30)
     ax.set_ylabel('seconds listened', fontsize=30)
-    fig.suptitle('plot mapping hours to seconds listened', fontsize=30)
+    fig.suptitle('seconds listened to music in each of\nthe past {} hours'.format(hours_cnt), fontsize=30)
     plt.show()
