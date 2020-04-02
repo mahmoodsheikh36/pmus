3e2c5a6c2817738d61e239ade4de2af30c50c3cc
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-02 11:39:43 +0300

    change the attribute names to be compatible with the new changes in the library

--- visualize.py
+++ visualize.py
@@ -19,7 +19,7 @@ def get_largest_elements(list_to_sort, limit, compare):
 
 def get_top_songs(provider, limit):
     def compare(song1, song2):
-        if song1.seconds_listened > song2.seconds_listened:
+        if song1.time_listened() > song2.time_listened():
             return True
         return False
     return get_largest_elements(provider.get_songs_list(),
@@ -28,7 +28,7 @@ def get_top_songs(provider, limit):
 
 def get_top_albums(provider, limit):
     def compare(album1, album2):
-        if album1.seconds_listened() > album2.seconds_listened():
+        if album1.time_listened() > album2.time_listened():
             return True
         return False
     return get_largest_elements(provider.get_albums_list(),
@@ -37,7 +37,7 @@ def get_top_albums(provider, limit):
 
 def get_top_artists(provider, limit):
     def compare(artist1, artist2):
-        if artist1.seconds_listened() > artist2.seconds_listened():
+        if artist1.time_listened() > artist2.time_listened():
             return True
         return False
     return get_largest_elements(provider.get_artists_list(),
@@ -52,9 +52,6 @@ if __name__ == '__main__':
     top_songs = get_top_songs(provider, ENTRY_COUNT)
     top_artists = get_top_artists(provider, ENTRY_COUNT)
 
-    # here starts matplotlib
-    #plt.figure(figsize=(1, 1))
-
     fig, (songs_graph, albums_graph, artists_graph) = plt.subplots(3)
     plt.subplots_adjust(hspace=0.5)
 
@@ -62,7 +59,7 @@ if __name__ == '__main__':
     songs_graph_color = (0.9, 0.4, 0.6)
     songs_graph.bar(['{}\n{}'.format(song.name, song.artists[0].name)
                      for song in top_songs],
-                    [song.seconds_listened for song in top_songs],
+                    [song.time_listened() / 1000 for song in top_songs],
                     color=songs_graph_color)
     songs_graph.set_xlabel('songs', fontsize=18, color=songs_graph_color,
                             weight='bold')
@@ -73,7 +70,7 @@ if __name__ == '__main__':
     albums_graph_color = (0.5, 0.5, 0.4)
     albums_graph.bar(['{}\n{}'.format(album.name, album.artists[0].name)
                       for album in top_albums],
-                     [album.seconds_listened() for album in top_albums],
+                     [album.time_listened() / 1000 for album in top_albums],
                      color=albums_graph_color)
     albums_graph.set_xlabel('albums', fontsize=18, color=albums_graph_color,
                             weight='bold')
@@ -83,7 +80,7 @@ if __name__ == '__main__':
     # plot artists
     artists_graph_color=(0.3, 0.3, 0.9)
     artists_graph.bar([artist.name for artist in top_artists],
-                      [artist.seconds_listened() for artist in top_artists],
+                      [artist.time_listened() / 1000 for artist in top_artists],
                       color=artists_graph_color)
     artists_graph.set_xlabel('artists', fontsize=18, color=artists_graph_color,
                             weight='bold')
