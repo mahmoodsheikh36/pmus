2a731b74c43be9a63274e2d86cb90e7aef1febb0
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-04 01:07:24 +0300

    this script is more generic now and plots the whole data not in hours but in milliseconds

--- plot_seconds_listened_to_hour.py
+++ plot_seconds_listened_to_hour.py
@@ -6,31 +6,30 @@ from music_daemon.utils import current_time
 
 plt.style.use(['dark_background'])
 
-def next_hour(time):
-    return time + 3600 * 1000
-
 if __name__ == '__main__':
     provider = MusicProvider()
     provider.load_music()
     first_listen_time = provider.playbacks[1].time_started
+    total_time = current_time() - first_listen_time
 
-    hours_cnt = int((current_time() - first_listen_time) / 1000 / 3600)
-    times_listened = []
+    time_fraction = 200
+    xs = []
+    ys = []
 
     from_time = first_listen_time
-    to_time = next_hour(from_time)
-    for i in range(hours_cnt):
-        total_time_listened = 0
+    to_time = from_time + total_time / time_fraction
+    for i in range(time_fraction):
+        time_listened = 0
         for song in provider.get_songs_list():
-            total_time_listened += song.time_listened(from_time, to_time)
-        times_listened.append(total_time_listened)
+            time_listened += song.time_listened(from_time, to_time)
+        xs.append(i)
+        ys.append(time_listened)
         from_time = to_time
-        to_time = next_hour(from_time)
+        to_time = from_time + total_time / time_fraction
 
     fig, ax = plt.subplots()
-    ax.plot([hour_num for hour_num in range(hours_cnt)],
-            [time_listened / 1000 for time_listened in times_listened], 'o-')
-    ax.set_xlabel('hour', fontsize=30)
-    ax.set_ylabel('seconds listened', fontsize=30)
-    fig.suptitle('seconds listened to music in each of\nthe past {} hours'.format(hours_cnt), fontsize=30)
+    ax.plot(xs, ys)
+    #ax.set_xlabel('time', fontsize=30)
+    #ax.set_ylabel('seconds listened', fontsize=30)
+    fig.suptitle('music routine', fontsize=30)
     plt.show()
