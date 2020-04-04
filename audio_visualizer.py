e84ef11353a2bacbf5cdc72b3d66882c760421f1
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-04 16:51:09 +0300

    experimenting

--- audio_visualizer.py
+++ audio_visualizer.py
@@ -9,12 +9,13 @@ plt.style.use(['dark_background'])
 if __name__ == '__main__':
 
     fig = plt.figure(figsize=(20, 3))
-    fig.tight_layout()
     ax = plt.axes()
 
-    spike_limit = 20
+    spike_limit = 40
 
+    final_samples = []
     def animate(i):
+        global final_samples
         try:
             with open('/home/mahmooz/.cache/music_daemon/audio_data.raw', 'rb') as f:
                 audio_bytes = f.read()
@@ -28,7 +29,6 @@ if __name__ == '__main__':
             audio_samples.append(audio_sample)
             idx += 2
 
-        final_samples = []
         idx = 0
         for i in range(spike_limit):
             samples_to_combine = audio_samples[idx:idx+spike_limit]
@@ -38,13 +38,16 @@ if __name__ == '__main__':
             final_samples.append(combined_sample)
             idx += spike_limit
 
+        final_samples = final_samples[-spike_limit*2:]
         X = np.linspace(0, 1, len(final_samples))
-        return ax.bar(X, final_samples, align='edge', width=0.05)
+        #return ax.bar(X, final_samples, align='edge', width=0.05)
+        fig.tight_layout()
+        return ax.plot(X, final_samples)
 
     plt.axis('off')
     ax.set_yticks([])
     ax.set_xticks([])
 
-    anim = FuncAnimation(fig, animate, interval=200, blit=True)
+    anim = FuncAnimation(fig, animate, interval=100, blit=True)
 
     plt.show()
