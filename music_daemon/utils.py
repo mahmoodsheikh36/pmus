03b1fa5cb929ebb22fb59b859637bb93f69ab2df
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-06 22:30:02 +0300

    add function to get home dir

--- music_daemon/utils.py
+++ music_daemon/utils.py
@@ -1,9 +1,13 @@
 import time
 import subprocess
 import os.path
+from pathlib import Path
 
 # time since the unix epoch in milliseconds
 current_time = lambda: int(round(time.time() * 1000))
 
 def file_exists(filepath):
     return os.path.isfile(filepath)
+
+def get_home_dir():
+    return str(Path.home())
