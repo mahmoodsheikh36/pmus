e496d792880a474be1adff02047ec3b2dfcb0aa2
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-08 13:51:40 +0300

    ignore songs that already have lyrics, remove ' from song names when fetching them from genius.com

--- scripts/fetch_lyrics.py
+++ scripts/fetch_lyrics.py
@@ -12,8 +12,8 @@ CONCURRENT_WORKERS = 200
 def get_lyrics(song):
     song_name, artist_name = song.name, song.artists[0].name
     url = "https://genius.com/{}-{}-lyrics".format(
-            artist_name.replace(' ', '-').capitalize(),
-            song_name.replace(' ', '-'))
+            artist_name.replace("'", "").replace(' ', '-').capitalize(),
+            song_name.replace("'", "").replace(' ', '-'))
     bs = BeautifulSoup(requests.get(url).content.decode(), 'html.parser')
     lyrics = bs.find_all("div", {"class": "lyrics"})[0].find_all('p')[0].text
     on_lyrics(song, lyrics)
@@ -27,7 +27,8 @@ if __name__ == '__main__':
 
     with ThreadPoolExecutor(max_workers=CONCURRENT_WORKERS) as executor:
         for song in music_provider.get_songs_list():
-            executor.submit(get_lyrics, song)
+            if music_provider.db_provider.get_song_lyrics(song.id) is None:
+                executor.submit(get_lyrics, song)
 
     print('done, saving to database')
     music_provider.db_provider.commit()
