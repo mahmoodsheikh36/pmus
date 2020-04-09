9f44ae667206841cdd3e566475bc66fb1f58e220
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-09 20:50:41 +0300

    show song name, image, artist name in a notification when a song is played

--- scripts/on_play.sh
+++ scripts/on_play.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/sh
 
-notify-send "song played $1"
+song_id="$1"
+song_path=$(md.py -r "info song $song_id url")
+song_name=$(md.py -r "info song $song_id name")
+artist_name=$(md.py -r "info song $song_id artist")
+ffmpeg -y -i "$song_path" /tmp/song_image.jpg && {
+    notify-send -i /tmp/song_image.jpg "$song_name" "$artist_name"
+}
