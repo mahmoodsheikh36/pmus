35749800450d0ffd92a7f13f240d3a752c46f0c3
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-09 21:08:54 +0300

    extract to png not jpg because it might be faster

--- scripts/on_play.sh
+++ scripts/on_play.sh
@@ -4,6 +4,6 @@ song_id="$1"
 song_path=$(md.py -r "info song $song_id url")
 song_name=$(md.py -r "info song $song_id name")
 artist_name=$(md.py -r "info song $song_id artist")
-ffmpeg -y -i "$song_path" /tmp/song_image.jpg && {
-    notify-send -i /tmp/song_image.jpg "$song_name" "$artist_name"
+ffmpeg -y -i "$song_path" /tmp/song_image.png && {
+    notify-send -i /tmp/song_image.png "$song_name" "$artist_name"
 }
