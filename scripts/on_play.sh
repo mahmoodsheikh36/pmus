43154c5664b3c5dbdc873be2b1dc37f72622f259
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-11 00:01:16 +0300

    silence ffmpeg

--- scripts/on_play.sh
+++ scripts/on_play.sh
@@ -4,6 +4,6 @@ song_id="$1"
 song_path=$(md.py -r "info song $song_id url")
 song_name=$(md.py -r "info song $song_id name")
 artist_name=$(md.py -r "info song $song_id artist")
-ffmpeg -y -i "$song_path" /tmp/song_image.png && {
+ffmpeg -y -loglevel panic -i "$song_path" /tmp/song_image.png && {
     notify-send -i /tmp/song_image.png "$song_name" "$artist_name"
 }
