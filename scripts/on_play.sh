f6e93cf0d857f92b2b8da2b4668e104ae8d8cdcc
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-11 19:22:51 +0300

    show artist and album not just artist

--- scripts/on_play.sh
+++ scripts/on_play.sh
@@ -3,7 +3,7 @@
 song_id="$1"
 song_path=$(md.py -r "info song $song_id url")
 song_name=$(md.py -r "info song $song_id name")
-artist_name=$(md.py -r "info song $song_id artist")
+artist_album=$(md.py -r "info song $song_id artist - album")
 ffmpeg -y -loglevel panic -i "$song_path" /tmp/song_image.png && {
-    notify-send -i /tmp/song_image.png "$song_name" "$artist_name"
+    notify-send -i /tmp/song_image.png "$song_name" "$artist_album"
 }
