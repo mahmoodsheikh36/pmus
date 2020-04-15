52fd86053962c709011eb4817bc04f14c6e6d139
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-15 18:29:00 +0300

    make use of other command line options instead of raw server message

--- scripts/on_play.sh
+++ scripts/on_play.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/sh
 
 song_id="$1"
-song_path=$(md.py -r "info song $song_id url")
-song_name=$(md.py -r "info song $song_id name")
-artist_album=$(md.py -r "info song $song_id artist - album")
+song_path=$(md.py -I url -S $song_id -o song)
+song_name=$(md.py -I name -S $song_id -o song)
+artist_album=$(md.py -I 'artist - album' -S $song_id -o song)
+echo $song_path
 ffmpeg -y -loglevel panic -i "$song_path" /tmp/song_image.png && {
     notify-send -i /tmp/song_image.png "$song_name" "$artist_album"
 }
