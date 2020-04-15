c2d99c2d0daf50ebeca9278918f6db738a455e62
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-15 08:23:20 +0300

    replace -i (ids) options with -S option (stands for specifier) and add sort_by to the info message

--- md.py
+++ md.py
@@ -45,8 +45,8 @@ if __name__ == '__main__':
                         action='store_true')
     parser.add_argument('-r', '--raw_cmd', metavar='raw command',
                         help='send a raw command to the server/daemon')
-    parser.add_argument('-i', '--ids', metavar='raw command',
-                        help='a comma-seperated list of ids of the music objects, or all/current to specify all objects or the currently playing object')
+    parser.add_argument('-S', '--specifier',
+                        help='the specifier, could be a list of song ids')
     parser.add_argument('-c', '--current', action='store_true',
                         dest='print_current_song',
                         help='print the current song (that is playing)')
@@ -54,7 +54,9 @@ if __name__ == '__main__':
                         help='tell the daemon to look for music')
     parser.add_argument('-I', '--info', metavar='info format', nargs='?',
                         const='id name\n',
-                        help='get info about objects of type specified by -o/--object and specify which objects to select using -i/--ids')
+                        help='get info about objects of type specified by -o/--object and specify which objects to select using -s/--specifier')
+    parser.add_argument('-s', '--sort_by', help='what to sort music objects by', 
+                        choices=('time_liked', 'name'))
     parser.add_argument('-P', '--port', help='network port to listen on',
                         type=int)
     parser.add_argument('-H', '--host', help='network host to listen on')
@@ -76,12 +78,13 @@ if __name__ == '__main__':
             cmd_to_stdout('find_music {}'.format(args.find_music))
         elif args.music_object:
             if args.info: # args.info contains the info format
-                cmd_to_stdout('info {} {} {}'.format(args.music_object,
-                                                     args.ids,
+                cmd_to_stdout('info {} {} {} {}'.format(args.music_object,
+                                                     args.specifier,
+                                                     args.sort_by,
                                                      args.info))
             elif args.play:
                 cmd_to_stdout('play {} {}'.format(args.music_object,
-                                                  args.ids.replace(',', ' ')))
+                                                  args.specifier.replace(',', ' ')))
             else:
                 parser.print_help()
         elif args.print_current_song:
