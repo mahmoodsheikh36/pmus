e585853a71e18dd2d9e397b3f8eb0d8246b4364c
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-16 23:24:15 +0300

    add function to send command to server and get full response

--- music_daemon/client.py
+++ music_daemon/client.py
@@ -17,6 +17,12 @@ def send_cmd(cmd, host=config.host, port=config.port):
         except UnicodeDecodeError:
             data += s.recv(RECV_CHUNK_SIZE)
 
+def send_cmd_wait_all(cmd, host=config.host, port=config.port):
+    all_data = ''
+    for data in send_cmd(cmd, host, port):
+        all_data += data
+    return all_data
+
 def cmd_to_stdout(cmd, host=config.host, port=config.port):
     for data in send_cmd(cmd, host, port):
         print(data, end='', flush=True)
