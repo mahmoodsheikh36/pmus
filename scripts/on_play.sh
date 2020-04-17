cc4371ace12d25e719ada491875c5671c86e50f5
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-17 04:47:38 +0300

    use artist_name/album_name instead of artist/album

--- scripts/on_play.sh
+++ scripts/on_play.sh
@@ -3,7 +3,7 @@
 song_id="$1"
 song_path=$(md.py -I url -S $song_id -o song)
 song_name=$(md.py -I name -S $song_id -o song)
-artist_album=$(md.py -I 'artist - album' -S $song_id -o song)
+artist_album=$(md.py -I 'artist_name - album_name' -S $song_id -o song)
 echo $song_path
 ffmpeg -y -loglevel panic -i "$song_path" /tmp/song_image.png && {
     notify-send -i /tmp/song_image.png "$song_name" "$artist_album"
