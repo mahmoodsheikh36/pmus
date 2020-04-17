ddb7dfd8d77d48341124f2c548ac521d7da3114f
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-17 19:41:33 +0300

    add function to replace multiple strings at once

--- music_daemon/utils.py
+++ music_daemon/utils.py
@@ -1,6 +1,7 @@
 import time
 import subprocess
 import os.path
+import re
 from pathlib import Path
 
 # time since the unix epoch in milliseconds
@@ -11,3 +12,7 @@ def file_exists(filepath):
 
 def get_home_dir():
     return str(Path.home())
+
+def multiple_replace(string, rep_dict):
+    pattern = re.compile("|".join([re.escape(k) for k in sorted(rep_dict,key=len,reverse=True)]), flags=re.DOTALL)
+    return pattern.sub(lambda x: rep_dict[x.group(0)], string)
