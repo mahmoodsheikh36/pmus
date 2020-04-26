8e9593c7d480bc2a2efb8b966539fdcf2e40e161
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-26 23:53:41 +0300

    use the new -F flag with -I

--- scripts/on_play.sh
+++ scripts/on_play.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
 song_id="$1"
-song_path=$(pmus -I url -S $song_id -o song)
-song_name=$(pmus -I name -S $song_id -o song)
-artist_album=$(pmus -I 'artist_name - album_name' -S $song_id -o song)
+song_path=$(pmus -I -F url -S $song_id -o song)
+song_name=$(pmus -I -F name -S $song_id -o song)
+artist_album=$(pmus -I -F 'artist_name - album_name' -S $song_id -o song)
 echo $song_path
 ffmpeg -y -loglevel panic -i "$song_path" /tmp/song_image.png && {
     notify-send -i /tmp/song_image.png "$song_name" "$artist_album"
